87a00b2327f68c8fc08e76e1c6fc605bd196bda7
Liam Gulliver <128083+lgulliver@users.noreply.github.com>
2019-12-18 11:40:59 +0000

    Purchase controller. Updated startup.cs in projects

---
+++ CarStorePurchase/Controllers/PurchaseController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarStorePurchase.Data;
+using CarStoreShared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CarStorePurchase.Controllers
+{
+    [Route("api/[controller]")]
+    public class PurchaseController : Controller
+    {
+        private readonly ApiOptions _apiOptions;
+        private readonly PurchaseOptions _purchaseOptions;
+        private PurchaseBroker _broker;
+
+
+        public PurchaseController(IOptions<ApiOptions> apiOptions, IOptions<PurchaseOptions> purchaseOptions)
+        {
+            _apiOptions = apiOptions.Value;
+            _purchaseOptions = purchaseOptions.Value;
+            _broker = new PurchaseBroker();
+        }
+
+        [HttpPost]
+        public string Post([FromBody]ApiPackage package)
+        {
+            var result = _broker.CheckoutCart(package.SessionIdentifier, package.ContentItem,
+                _purchaseOptions.SendGridApiKey, _purchaseOptions.CartApi, _purchaseOptions.CartApiKey,
+                _purchaseOptions.InventoryApi, _purchaseOptions.InventoryApiKey);
+
+            return result;
+        }
+    }
+}
--- CarStorePurchase/Startup.cs
+++ CarStorePurchase/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarStorePurchase.Data;
+using CarStoreShared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,10 @@ namespace CarStorePurchase
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOptions();
+
+            services.Configure<ApiOptions>(Configuration);
+            services.Configure<PurchaseOptions>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
